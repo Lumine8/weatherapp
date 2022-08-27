5aed96ee3ad442d45738c0e27c2d7913ec69c02c
Lumine8 <sankargopan1@gmail.com>
2022-08-27 23:28:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });
     }
   }
 
